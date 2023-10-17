6686135532050b129cb0a90d5e9527d190e655a6
asaenzl <92343724+asaenzl@users.noreply.github.com>
2023-10-16 22:37:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("My Parents New Healthy Dinner")
 
-streamlit.header('🥣Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
