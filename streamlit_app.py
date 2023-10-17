b3574a8c11cde6731400e2de136b7299336fd407
asaenzl <92343724+asaenzl@users.noreply.github.com>
2023-10-16 22:36:08 -0600

    Update streamlit_app.py
    Adding menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st 
 
 st.title("My Parents New Healthy Dinner")
+
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
