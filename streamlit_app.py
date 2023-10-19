d6fd12e0d29e968d5183066cf5f637c904e27013
asaenzl <92343724+asaenzl@users.noreply.github.com>
2023-10-18 21:16:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
 st.dataframe(my_fruit_list)
