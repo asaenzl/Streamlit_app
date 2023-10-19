68ea73353a5b19ec22dd491f071cce713cec8131
asaenzl <92343724+asaenzl@users.noreply.github.com>
2023-10-18 20:52:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.header('🥣Breakfast Menu')
 st.text(' 🥗 Omega 3 & Blueberry Oatmeal')
 st.text(' 🐔 Kale, Spinach & Rocket Smoothie')
 st.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
